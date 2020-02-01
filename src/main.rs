7467b01444c6b9bdc4a7c8a7cb3520eb8265b476
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2020-02-01 21:49:22 +0100

    Fix bug: Accomodate bash prompt limitations by separating into two

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,34 @@
 extern crate ansi_term;
 
 use std::process::Command;
+use std::env;
 
-use ansi_term::Colour;
+const COLOR_RED : &str = "\u{001b}[0;31m";
+const COLOR_YELLOW : &str = "\u{001b}[0;33m";
+const COLOR_GREEN : &str = "\u{001b}[0;32m";
+const COLOR_OCHRE : &str = "\u{001b}[0;37m";
+const COLOR_RESET : &str = "\u{001b}[0m";
 
 fn main() {
+    let type_res = env::args().skip(1).next().unwrap();
     let output = Command::new("git").arg("status").output().expect("Failed to retrieve status from Git");
     let output = String::from_utf8_lossy(output.stdout.as_slice());
     let text = output.to_string();
-    let color = if !text.contains("working directory clean") {
-        Colour::Red
-    }else if text.contains("Your branch is ahead of") {
-        Colour::Yellow
-    }else if text.contains("nothing to commit"){
-        Colour::Green
-    }else {
-        Colour::Purple
-    };
-    if let Some(branch_info) = text.lines().next(){
-        let branch_info = branch_info.replace("On branch ","").replace("HEAD detached at ", ""); //replace only replaces if it finds any
-        let branch_info = format!("({})", branch_info);
-        println!("{}", color.paint(branch_info)); //branch-name or commit-id
+    if type_res == "branch" {
+        if let Some(branch_info) = text.lines().next(){
+            let branch_info = branch_info.replace("On branch ","").replace("HEAD detached at ", ""); //replace only replaces if it finds any
+            print!("({})", branch_info); //branch-name or commit-id
+        }
+    } else {
+        let color = if !text.contains("working directory clean") {
+            COLOR_RED
+        }else if text.contains("Your branch is ahead of") {
+            COLOR_YELLOW
+        }else if text.contains("nothing to commit"){
+            COLOR_GREEN
+        }else {
+            COLOR_OCHRE
+        };
+        print!("{}", color); //branch-name or commit-id
     }
 }
