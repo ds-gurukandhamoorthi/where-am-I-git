6291689a3698119ff02ea53aa02932f031b36bdb
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2020-02-01 17:14:31 +0100

    Fix bug: Remove confusion between first-line and whole-content

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,19 @@ use ansi_term::Colour;
 fn main() {
     let output = Command::new("git").arg("status").output().expect("Failed to retrieve status from Git");
     let output = String::from_utf8_lossy(output.stdout.as_slice());
-    let status_line = output.lines().next();
-    match status_line {
-        Some(status) => {
-            let status = status.replace("On branch ","").replace("HEAD detached at ", ""); //replace only replaces if it finds any
-            let color = if !status.contains("working directory clean") {
-                Colour::Red
-            }else if status.contains("Your branch is ahead of") {
-                Colour::Yellow
-            }else if status.contains("nothing to commit"){
-                Colour::Green
-            }else {
-                Colour::Purple
-            };
-            let status = format!("({})", status);
-            println!("{}", color.paint(status));
-        },
-        _ => println!(),
+    let text = output.to_string();
+    let color = if !text.contains("working directory clean") {
+        Colour::Red
+    }else if text.contains("Your branch is ahead of") {
+        Colour::Yellow
+    }else if text.contains("nothing to commit"){
+        Colour::Green
+    }else {
+        Colour::Purple
+    };
+    if let Some(branch_info) = text.lines().next(){
+        let branch_info = branch_info.replace("On branch ","").replace("HEAD detached at ", ""); //replace only replaces if it finds any
+        let branch_info = format!("({})", branch_info);
+        println!("{}", color.paint(branch_info)); //branch-name or commit-id
     }
 }
