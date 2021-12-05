cfc10146f5e00cfaf4ec0eadf269384cbc23f62c
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2021-12-05 22:22:17 +0100

    Modify: Output branch in color in a single call

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::process::Command;
-use std::env;
 
 const COLOR_RED : &str = "\u{001b}[0;31m";
 const COLOR_YELLOW : &str = "\u{001b}[0;33m";
@@ -8,27 +7,26 @@ const COLOR_OCHRE : &str = "\u{001b}[0;37m";
 const COLOR_RESET : &str = "\u{001b}[0m";
 
 fn main() {
-    let type_res = env::args().nth(1).unwrap();
-    if type_res == "branch" {
-        let output = Command::new("git").arg("rev-parse").arg("--abbrev-ref").arg("HEAD").output().expect("Failed to retrieve branch name from Git");
-        let output = String::from_utf8_lossy(output.stdout.as_slice());
-        let output = output.replace('\n', "");
-        if !output.is_empty(){
-            print!("({})", output); //branch-name or commit-id
+
+        //branch-name or commit-id
+        let branch_head_args = ["rev-parse", "--abbrev-ref", "HEAD"];
+        let output = Command::new("git").args(&branch_head_args).output().expect("Failed to retrieve branch name from Git");
+        let branch_name = String::from_utf8_lossy(output.stdout.as_slice());
+        let branch_name = branch_name.trim_end();
+        if !branch_name.is_empty(){
+            //color code
+            let output = Command::new("git").arg("status").output().expect("Failed to retrieve status from Git");
+            let output = String::from_utf8_lossy(output.stdout.as_slice());
+            let text = output.to_string();
+            let color = if !text.contains("working tree clean") {
+                COLOR_RED
+            }else if text.contains("Your branch is ahead of") {
+                COLOR_YELLOW
+            }else if text.contains("nothing to commit"){
+                COLOR_GREEN
+            }else {
+                COLOR_OCHRE
+            };
+            println!("{}({})", color, &branch_name)
         }
-    } else {
-        let output = Command::new("git").arg("status").output().expect("Failed to retrieve status from Git");
-        let output = String::from_utf8_lossy(output.stdout.as_slice());
-        let text = output.to_string();
-        let color = if !text.contains("working tree clean") {
-            COLOR_RED
-        }else if text.contains("Your branch is ahead of") {
-            COLOR_YELLOW
-        }else if text.contains("nothing to commit"){
-            COLOR_GREEN
-        }else {
-            COLOR_OCHRE
-        };
-        print!("{}", color); //branch-name or commit-id
-    }
 }
